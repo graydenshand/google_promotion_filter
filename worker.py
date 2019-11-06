c5299403fd44abe20635f7b572b76bbc52e19e4a
Grayden Shand <graydenshand@gmail.com>
2019-11-06 15:27:47 -0500

    updating procfile

--- worker.py
+++ worker.py
@@ -6,7 +6,7 @@ from rq import Worker, Queue, Connection
 listen = ['high', 'default', 'low']
 # listen = ['default']
 
-redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
 
 conn = redis.from_url(redis_url)
 
